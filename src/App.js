81c1df57e45797c18fe11d11dbe82172c7816239
joaofaria-src <17826036+joaofaria-src@users.noreply.github.com>
2024-12-17 14:45:25 +0000

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Nav from './Nav';
 import Main from './Main';
